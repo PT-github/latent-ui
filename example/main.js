8e20a6ce4c9ed8849e8f3ebc48d6f3d83016607d
pengtao <pengtao_it@163.com>
2020-04-27 17:46:48 +0800

    修改css打包输出目录2

--- example/main.js
+++ example/main.js
@@ -2,7 +2,7 @@
  * @Author: PT
  * @Date: 2020-04-23 09:33:57
  * @LastEditors: PT
- * @LastEditTime: 2020-04-26 11:40:54
+ * @LastEditTime: 2020-04-27 17:45:40
  * @Description: 入口文件
  */
 import Vue from 'vue'
@@ -10,7 +10,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' 
 import EleUi from '../lib'
-import '../lib/index.css'
+import '../lib/theme/index.css'
 import 'highlight.js/styles/color-brewer.css'
 import './asset/style/index.less'
 import router from './router'
